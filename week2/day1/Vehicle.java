58b1e6402785df1fde004da52d0c0f326c7bb1b8
Justin <jrajasekar@ntst.com>
2022-05-08 11:25:11 +0530

    Initial version

---
+++ week2/day1/Vehicle.java
@@ -0,0 +1,14 @@
+package week2.day1;
+
+public class Vehicle extends Commute{
+public void acclerate(){
+	System.out.println(carName);
+	System.out.println("Speed up the vehicle");
+}
+public void applyBrake() {
+	System.out.println("Slow down the vehicle");
+}
+public void soundHorn() {
+	System.out.println("Make some noise");
+}
+}
